84be9c2d6f26df9e00deee69f6ce76b9b6b0ad43
Алексей Лисичников <alexochanel@gmail.com>
2023-04-29 20:16:52 +0300

    пока так

---
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] places = { "10 место", "9 место", "8 место", "7 место", "6 место", "5 место", "4 место", "3 место", "2 место", "1 место",};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.igorek);
+        imageView.setImageResource(R.drawable.igor);
+
+        ListView animeList = findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.item_prikol, R.id.text_item, places);
+        animeList.setAdapter(adapter);
+        animeList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, Place_Ten.class);
+                    startActivity(intent);
+                }
+                if (position == 1) {
+                    Intent intent = new Intent(MainActivity.this, Place_Nine.class);
+                    startActivity(intent);
+                }
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/list/Place_Ten.java
@@ -0,0 +1,14 @@
+package com.example.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Place_Ten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_ten);
+    }
+}
